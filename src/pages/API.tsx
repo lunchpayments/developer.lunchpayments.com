8f09616c9631d06a5d0e1e5bbf9c4fe7db25eed3
Ben Fox <work@nullfox.com>
2024-08-05 16:24:41 -0700

    - Remove old html file and add react swagger

--- src/pages/API.tsx
+++ src/pages/API.tsx
@@ -1,19 +1,18 @@
-import { useHref, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export const API = () => {
-  const source = useHref("/swagger/index.html").replace("#", "");
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
 
-  return (
-    <div className="h-screen w-screen">
-      <div className="bg-brand py-3">
-        <div className="px-[20px] mx-auto max-w-[1460px]">
-          <Link to=".." className="text-white font-semibold hover:opacity-75">
-            Back
-          </Link>
-        </div>
+export const API = () => (
+  <div className="h-screen w-screen">
+    <div className="bg-brand py-3">
+      <div className="px-[20px] mx-auto max-w-[1460px]">
+        <Link to=".." className="text-white font-semibold hover:opacity-75">
+          Back
+        </Link>
       </div>
-
-      <iframe src={source} width="100%" height="100%" frameBorder="0"></iframe>
     </div>
-  );
-};
+
+    <SwaggerUI url="/swagger/openapi.yaml" />
+  </div>
+);
